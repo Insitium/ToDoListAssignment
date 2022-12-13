ad548b5ef3b1b99271647b661521bb195ae25421
Sarthak Vashistha <vashistha.sarthak31@gmail.com>
2022-12-13 00:49:58 -0500

    internal documentation Added

--- ToDoListAssignment/NotesData.swift
+++ ToDoListAssignment/NotesData.swift
@@ -1,9 +1,16 @@
 //
 //  NotesData.swift
 //  ToDoListAssignment
-//
-//  Created by Sarthak Vashistha on 2022-12-12.
-//
+/* Created and Developed by
+ Manmeen Kaur - 301259638
+ Sarthak Vashistha - 301245284
+ Neha Patel - 301280513
+ 
+ 
+ Date Created: 11/12/2022
+ To-Do List Assignment - Created To Do List App - Data Persistence + Gesture Control
+ Version: 1.3.0
+ */
 
 import CoreData
 
--- ToDoListAssignment/ToDoTableView.swift
+++ ToDoListAssignment/ToDoTableView.swift
@@ -1,10 +1,16 @@
 //
 //  ToDoTableView.swift
 //  ToDoListAssignment
-//
-//  Created by Sarthak Vashistha on 2022-12-11.
-//
-
+/* Created and Developed by
+ Manmeen Kaur - 301259638
+ Sarthak Vashistha - 301245284
+ Neha Patel - 301280513
+ 
+ 
+ Date Created: 11/12/2022
+ To-Do List Assignment - Created To Do List App - Data Persistence + Gesture Control
+ Version: 1.3.0
+ */
 import UIKit
 import CoreData
 
